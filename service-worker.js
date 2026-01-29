c66ce6b5dfbb4046a8bab717d9dd06cd2d3a1fc0
hydddros <adrhydros@gmail.com>
2026-01-29 20:13:16 +0800

    Update service worker cache and assets
    Updated CACHE_NAME to v2 and added new icons to ASSETS.

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,21 @@
-const CACHE_NAME = 'siyasatph-v1';
+const CACHE_NAME = 'siyasatph-v2'; // Changed to v2 to force an update
 
 const ASSETS = [
   '/siyasatTest/',
   '/siyasatTest/index.html',
-  '/siyasatTest/manifest.json'
+  '/siyasatTest/manifest.json',
+  '/siyasatTest/icons/icon-192.png', // Add this
+  '/siyasatTest/icons/icon-512.png'  // Add this
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caching assets...');
+        return cache.addAll(ASSETS);
+      })
+      .catch(err => console.error('Cache Fail:', err)) // This will tell you if a file is missing
   );
 });
 
